e62cd117d40e894ef017d4aed596bb2aa915977e
Côme Terlecki <cometerlecki@protonmail.com>
2022-02-28 14:55:16 +0100

    modal handle error

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,21 +1,30 @@
 import './App.css';
-import Header from './modals_parcels/header'
-import Label from './modals_parcels/labelBar'
-import Home from './components/homepage'
-import { Menu, Dropdown, Tooltip, Affix, Image, Button } from 'antd';
-import {Link} from 'react-router-dom'
+import Header from './modals_parcels/header';
+import Label from './modals_parcels/labelBar';
+import Home from './components/homepage';
+import {Menu, Dropdown, Tooltip, Affix, Image, Button} from 'antd';
+import {Link} from 'react-router-dom';
+import SignModal from './modals_parcels/signModal';
+
+// REDUX
+import {Provider} from 'react-redux'
+import {createStore, combineReducers} from 'redux'
+
+// Generate store
+import token from './reducers/token.reducer'
+const store = createStore(combineReducers({token}))
 
 function App() {
 
-  
-  return (
-    <div style={{backgroundColor:"#FCF5EE", height : '100vh', fontFamily : 'Montserrat'}}>
+   return (
+      <Provider store={store}>
+         <div style={{backgroundColor: "#FCF5EE", height: '100vh', fontFamily: 'Montserrat'}}>
 
-        <Header />
-        <Label />
+            <SignModal/>
 
-    </div>
-  );
+         </div>
+      </Provider>
+   );
 }
 
 export default App;
--- reactapp/src/modals_parcels/signModal.js
+++ reactapp/src/modals_parcels/signModal.js
@@ -1,38 +1,40 @@
 // Import react and react dependencies
-import React from 'react';
-import {useState} from "react";
+import React, {useState} from 'react';
 
 // Import Icon from Ant Design Icon
 import {CloseOutlined, EyeInvisibleOutlined, EyeOutlined} from "@ant-design/icons";
 
 // Import style
-import '../../public/stylesheets/signModal.css';
+import '../stylesheets/signModal.css';
+import {connect} from "react-redux";
 
-export default function signModal() {
-
+function SignModal() {
    // Set Modal visible or not
    const [isSignModalVisible, setIsSignModalVisible] = useState('hidden');
 
    // Inputs
-   const [registerEmailAdress, setRegisterEmailAdress] = useState('');
-   const [loginEmailAdress, setLoginEmailAdress] = useState('');
-   const [registerPassword, setRegisterPassword] = useState('');
-   const [loginPassword, setLoginPassword] = useState('');
    const [firstName, setFirstName] = useState('');
    const [lastName, setLastName] = useState('');
+   const [registerEmailAdress, setRegisterEmailAdress] = useState('');
+   const [registerPassword, setRegisterPassword] = useState('');
+   const [loginEmailAdress, setLoginEmailAdress] = useState('');
+   const [loginPassword, setLoginPassword] = useState('');
    const [cgu, setCGU] = useState(false);
 
    // Show or Hide password
    const [isLoginPasswordVisible, setIsLoginPasswordVisible] = useState("password");
    const [iconLogin, setIconLogin] = useState(<EyeOutlined
       className="iconPassword"
-      onClick={() => isPasswordVisible('login', 'password')}/>
+      onClick={() => isPasswordVisible('login', 'password')}/>,
    );
    const [isRegisterPasswordVisible, setIsRegisterPasswordVisible] = useState("password");
    const [iconRegister, setIconRegister] = useState(<EyeOutlined
       className="iconPassword"
-      onClick={() => isPasswordVisible('register', 'password')}/>
+      onClick={() => isPasswordVisible('register', 'password')}/>,
    );
+   // Error handlers
+   const [errorSignUp, setErrorSignup] = useState('');
+   const [errorSignin, setErrorSignin] = useState('');
 
    // Function for set password visible or not
    var isPasswordVisible = (sign, inputType) => {
@@ -40,29 +42,60 @@ export default function signModal() {
          if (inputType === 'password') {
             setIsRegisterPasswordVisible('text');
             setIconRegister(<EyeInvisibleOutlined className="iconPassword"
-               onClick={() => isPasswordVisible('register', 'text')}/>);
+                                                  onClick={() => isPasswordVisible('register', 'text')}/>);
          } else {
             setIsRegisterPasswordVisible("password");
             setIconRegister(<EyeOutlined className="iconPassword"
-               onClick={() => isPasswordVisible('register', 'password')}/>);
+                                         onClick={() => isPasswordVisible('register', 'password')}/>);
          }
       } else if (sign === 'login') {
          if (inputType === "password") {
             setIsLoginPasswordVisible("text");
             setIconLogin(<EyeInvisibleOutlined className="iconPassword"
-               onClick={() => isPasswordVisible('login', 'text')}/>);
+                                               onClick={() => isPasswordVisible('login', 'text')}/>);
          } else {
             setIsLoginPasswordVisible("password");
             setIconLogin(<EyeOutlined className="iconPassword"
-               onClick={() => isPasswordVisible('login', 'password')}/>);
+                                      onClick={() => isPasswordVisible('login', 'password')}/>);
          }
+      }
+   };
+
+   var handleSignup = async () => {
+      let data = await fetch('/users/sign-up', {
+         method: 'POST',
+         headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+         body: `firstName=${firstName}&lastName=${lastName}&email=${registerEmailAdress}&password=${registerPassword}`,
+      });
+      data = await data.json();
+      if (data.token) {
+         console.log('redirect');
+      } else {
+         setErrorSignup(<p style={{color: "red"}}>{data.comment}</p>);
+
+      }
+   };
+
+   var handleSignin = async () => {
+      let data = await fetch('/users/sign-in', {
+         method: 'POST',
+         headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+         body: `email=${loginEmailAdress}&password=${loginPassword}`,
+      });
+      data = await data.json();
+      if (data.token) {
+         console.log('redirect');
+      } else {
+         setErrorSignin(<p style={{color: "red"}}>{data.comment}</p>);
+
       }
    };
 
    return (
       <div className="App">
          <button onClick={() => setIsSignModalVisible('visible')}>Show Modal</button>
-         <div className="modal" style={{visibility: isSignModalVisible}} onClick={() => setIsSignModalVisible('hidden')}>
+         <div className="modal" style={{visibility: isSignModalVisible}}
+              onClick={() => setIsSignModalVisible('hidden')}>
             <div className="sign" style={{zIndex: 25}} onClick={e => e.stopPropagation()}>
                <CloseOutlined
                   style={{position: "absolute", right: 3, top: 3, fontSize: "25px", cursor: "pointer"}}
@@ -70,17 +103,19 @@ export default function signModal() {
                />
                <div className="signSection">
                   <h5 className="signTitle">Heureux de vous revoir !</h5>
+                  {errorSignin}
                   <form>
-                     <input type="text" name="input" placeholder="Votre adresse email" className=""
+                     <input type="text" name="input" placeholder="Votre adresse email"
+                            className="" required={true}
                             onChange={(e) => setLoginEmailAdress(e.target.value)} value={loginEmailAdress}/>
                      <div className="passwordDiv">
                         <input type={isLoginPasswordVisible} name="input" placeholder="Créez un mot de passe"
-                               className=""
+                               className="" required={true}
                                onChange={(e) => setLoginPassword(e.target.value)} value={loginPassword}/>
                         {iconLogin}
                      </div>
                   </form>
-                  <button onClick={() => setIsSignModalVisible('hidden')} className="mt">Se connecter</button>
+                  <button onClick={() => handleSignin()} className="mt">Se connecter</button>
                   <a href=""><p className="forgetPassword">mot de passe oublié ? c’est par ici 👇</p></a>
                   <h5 className="signTitle mt">Ou connectez-vous via : </h5>
                   <img style={{width: "270px", height: "50px"}} alt="google auth picture"
@@ -89,21 +124,22 @@ export default function signModal() {
                <span className="divider"/>
                <div className="signSection">
                   <h5 className="signTitle">Inscrivez-vous 😊</h5>
+                  {errorSignUp}
                   <form>
-                     <input type="text" name="input" placeholder="* Votre prénom" className=""
+                     <input type="text" name="input" placeholder="* Votre prénom" className="" required={true}
                             onChange={(e) => setFirstName(e.target.value)} value={firstName}/>
-                     <input type="text" name="input" placeholder="* Votre nom" className=""
+                     <input type="text" name="input" placeholder="* Votre nom" className="" required={true}
                             onChange={(e) => setLastName(e.target.value)} value={lastName}/>
-                     <input type="text" name="input" placeholder="* Votre adresse email" className=""
+                     <input type="text" name="input" placeholder="* Votre adresse email" className="" required={true}
                             onChange={(e) => setRegisterEmailAdress(e.target.value)} value={registerEmailAdress}/>
                      <div className="passwordDiv">
                         <input type={isRegisterPasswordVisible} name="input" placeholder="* Créez un mot de passe"
-                               className=""
+                               className="" required={true}
                                onChange={(e) => setRegisterPassword(e.target.value)} value={registerPassword}/>
                         {iconRegister}
                      </div>
                      <div className="raw">
-                        <input type="checkbox" name="checkbox"
+                        <input type="checkbox" name="checkbox" required={true}
                                defaultChecked={cgu} onChange={() => setCGU(!cgu)}/>
                         <label htmlFor="checkbox">J'accepte les Conditions Générales de Vente et la politique de
                            confidentialité </label>
@@ -111,7 +147,7 @@ export default function signModal() {
                   </form>
                   <div className="submitRegister">
                      <p>*Champs Requis</p>
-                     <button onClick={() => setIsSignModalVisible('hidden')}>S'inscrire</button>
+                     <button onClick={() => handleSignup()}>S'inscrire</button>
                   </div>
                </div>
             </div>
@@ -119,3 +155,16 @@ export default function signModal() {
       </div>
    );
 }
+
+function mapDispatchToProps(dispatch) {
+   return {
+      addToken: function (token) {
+         dispatch({type: 'addToken', token: token});
+      },
+   };
+}
+
+export default connect(
+   null,
+   mapDispatchToProps,
+)(SignModal);
