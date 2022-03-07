68a356f56faf5570fe58d01a9d799bbf256416c8
Mathieu Pereira <mathieu.pereira&live.fr>
2022-03-07 09:30:25 +0100

    resize img and countdown

--- reactapp/src/components/productPage.js
+++ reactapp/src/components/productPage.js
@@ -97,7 +97,7 @@ function ProductPage(props) {
 
                    {/* Concerne l'image de la marque*/}
                   <div style={{width: 250, height: 250, position: 'relative', marginBottom : 10}}>
-                     <img style={{width: "250px", height: "250px"}} src={`/assets/${saleImg}.jpeg`}
+                     <img style={{width: "250px", height: "250px"}} src={`/assets/${saleImg}_sale.jpeg`}
                           alt="picture"/>
 
                      <div style={{position: 'absolute', top: 10, right: 10, display: "flex", flexDirection: "column"}}>
