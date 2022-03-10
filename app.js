d9213dd24cb10103f351ce043d6afdb86e4ba8d1
Mathieu Pereira <mathieu.pereira&live.fr>
2022-03-10 15:16:35 +0100

    affichage sales ok

--- app.js
+++ app.js
@@ -19,12 +19,12 @@ app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'reactapp/build')));
 
-// app.get('/*', function (req, res) {
-//     res.sendFile(path.join(__dirname, "reactapp/build/index.html"));
-// });
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/sales', salesRouter);
+
+app.get('/*', function (req, res) {
+    res.sendFile(path.join(__dirname, "reactapp/build/index.html"));
+});
 
 module.exports = app;
